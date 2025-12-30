fc24d4bb1ad7a578fd8d1bc98b4200ff06ff8474
Anantha Atole <atoleanantha03@gmail.com>
2025-12-30 22:20:08 +0530

    cart slice and logic added cart page modified

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,11 @@
 // confing store
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlices';
-
+import cartSlice from './slices/cartSlice';
 const store = configureStore({
     reducer: {
         auth: authReducer,
+         cart: cartSlice,
     },
 });
 
